fa986cfd393f862d4df21793460d4827a8d84098
ahmed Qasem <abok2ssem14@gmail.com>
2024-04-23 23:02:01 +0200

    Load the LoadingScreen

--- App.tsx
+++ App.tsx
@@ -13,64 +13,73 @@ import RegistrationScreen from "./app/screens/RegistrationScreen";
 import ForgetPasswordScreen from "./app/screens/ForgetPasswordScreen";
 import EmergencyScreen from "./app/screens/EmergencyScreen";
 import { HomeScreenNavigateProps } from "./type";
+import { useEffect, useState } from "react";
 
 const Stack = createNativeStackNavigator<HomeScreenNavigateProps>();
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  useEffect(() => {
+    setTimeout(() => setIsLoading(!isLoading), 3000);
+  },[]);
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Register"
-          component={RegistrationScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="ForgetPassword"
-          component={ForgetPasswordScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Emergency"
-          component={EmergencyScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="HeartRate"
-          component={HeartRateScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Temperature"
-          component={TemperatureScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Oxygen"
-          component={OxygenScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="EditProfile"
-          component={EditProfileScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="MedicineAlarm"
-          component={MedicineAlarmScreen}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
+      {isLoading ? (
+        <LoadingScreen />
+      ) : (
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Register"
+            component={RegistrationScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="ForgetPassword"
+            component={ForgetPasswordScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Emergency"
+            component={EmergencyScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="HeartRate"
+            component={HeartRateScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Temperature"
+            component={TemperatureScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Oxygen"
+            component={OxygenScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="EditProfile"
+            component={EditProfileScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="MedicineAlarm"
+            component={MedicineAlarmScreen}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 }
